c0392f2239a5eb833b2fcec987760afa2eb97b42
Angel <aleangrodriguez123@gmail.com>
2018-10-13 08:28:11 -0500

    new build

--- src/api/post.js
+++ src/api/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const HOST = process.env.ROOT_HOST || 'http://localhost:3000/api';
+const HOST = process.env.VUE_APP_ROOT_HOST || 'http://localhost:3000/api';
 
 export default {
     list() {
