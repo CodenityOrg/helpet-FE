3d71922dbe49059c02851eb85cbeae693cb08908
Angel <aleangrodriguez123@gmail.com>
2018-12-06 00:58:52 -0500

    adjusting calculate for scroll view

--- src/components/InfiniteList.vue
+++ src/components/InfiniteList.vue
@@ -12,14 +12,20 @@
     name: "InfiniteList",
     methods: {
       async scrollHandler() {
+        console.log("scrollHeight", this.$refs.container.scrollHeight);
         if (this.isAtTheBottom()) {
           console.log("scroll reached")
             this.$emit("scrollEnd");
         }
       },
+      mounted() {
+        console.log("scrollHeight", this.$refs.container.scrollHeight);
+      },
       isAtTheBottom() {
-            const container = this.$refs.container;
-            return (container.scrollHeight - container.scrollTop ) === (container.clientHeight );
+          const {clientHeight, scrollHeight, scrollTop} = this.$refs.container;
+          const diff = Math.trunc(scrollHeight - scrollTop);
+          console.log(diff, Math.trunc(this.$refs.container.getBoundingClientRect().height))
+          return diff === Math.trunc(this.$refs.container.getBoundingClientRect().height);
       }
     }
   }
