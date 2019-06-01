3e88a26380605f8969917bb5d30ad25c7ceba523
AngelRodro <aleangrodriguez123@gmail.com>
2019-06-01 16:48:49 -0500

    add missing files

---
+++ src/components/common/Map.vue
@@ -0,0 +1,39 @@
+<template>
+    <mapbox 
+        :access-token="accessToken"
+        :map-options="{
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [0, 0],
+            zoom: 8
+        }"
+        :geolocate-control="{
+            show: true,
+            position: 'top-left'
+        }"
+        :nav-control="{
+            show: true, 
+            position: 'top-left'
+        }"
+        @map-init="$emit('init', $event)"
+        @map-click="(map, position) => $emit('clicked', map, position)"
+        :fullscreen-control="{
+            show: true,
+            position: 'top-left'
+        }"
+    />
+</template>
+<script>
+    import Mapbox from 'mapbox-gl-vue';
+    
+    export default {
+        name: "PostMap",
+        components: {
+            Mapbox
+        },
+        data() {
+            return {
+                accessToken: "pk.eyJ1IjoiYW5nZWxyb2Rybzk1IiwiYSI6ImNqODljcTJrdDAxaWIyd21rNTZubHQwamMifQ.6ghwymwGfrRC15-iKOxcww"
+            }
+        }
+    }
+</script>
---
+++ src/views/mixins/map.js
@@ -0,0 +1,41 @@
+
+export default {
+    methods: {
+        mapInitialized(map) {
+            this.map = map;
+            map.on("load", function () {
+                if ("geolocation" in navigator) { 
+                    navigator.geolocation.getCurrentPosition(position => {
+                        document.querySelector(".mapboxgl-ctrl-geolocate").click();
+                    })
+                }
+            });
+            
+        },
+        genLayoutMarker(data) {
+            const el = document.createElement("div");
+            el.className = (data.type == 1) ? "marker marker--encontrado" : "xmarker marker--perdido";
+            if (data.photo) {
+                el.style.backgroundImage = `url(${data.photo})`;
+            }
+            el.dataset.idPet = data.id;
+            el.style.width = data.properties.iconSize[0] ? (data.properties.iconSize[0] + 'px') : '48px';
+            el.style.height = data.properties.iconSize[1] ? (data.properties.iconSize[1] + 'px') : '48px';
+            el.addEventListener("click",function(e){
+                const { target: { dataset: { idPet }}} = e;
+                const divPet = document.getElementById(idPet);
+                const tabLost = document.getElementById('tab-perdidos') || document.getElementById('tab-encontrados');
+                tabLost.scrollTop = divPet.offsetTop;
+            });
+            return el;
+        }
+    },
+    beforeDestroy() {
+        this.map.remove();
+    },
+    data() {
+        return {
+            map: {}
+        }
+    }
+}
