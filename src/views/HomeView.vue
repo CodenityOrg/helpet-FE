27cf4b6c0f62ad7ac61ba55c6a0be87f7bee6783
Angel <aleangrodriguez123@gmail.com>
2019-06-16 15:07:11 -0500

    home fullpage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,36 @@
     <div class="filtro cont">
         <router-link :to="{name : 'Home'}">
         <section class="cont--slogan">
-            <ul class="slogan__elemt">
-                <div class="slogan">
-                    <h3 class="slogan__title">
-                        Encuentra a tu <span class="slogan__title--bold">mascota</span>
-                        <br>
-                        perdida o ayuda a una a encontrar su hogar
-                    </h3>
-                    <span class="slogan__subtitle">Porque las mascotas son los mejores amigos</span>
-
-                    <router-link :to="{name : 'ListLost'}">Comienza</router-link>
-
-                </div>
-                <div class="logo">
-                    <img src="../assets/img/img-A.png" alt="logo helpet">
-                </div>
-            </ul>
+            <full-page ref="fullpage" :options="options">
+                <ul class="slogan__elemt section">
+                    <div class="slogan">
+                        <h3 class="slogan__title">
+                            Encuentra a tu <span class="slogan__title--bold">mascota</span>
+                            <br>
+                            perdida o ayuda a una a encontrar su hogar
+                        </h3>
+                        <span class="slogan__subtitle">Porque las mascotas son los mejores amigos</span>
+                        <router-link :to="{name : 'ListLost'}">Comienza</router-link>
+                    </div>
+                    <div class="logo">
+                        <img src="../assets/img/img-A.png" alt="logo helpet">
+                    </div>
+                </ul>
+                <ul class="slogan__elemt section">
+                    <div class="slogan">
+                        <h3 class="slogan__title">
+                            Encuentra a tu <span class="slogan__title--bold">mascota</span>
+                            <br>
+                            perdida o ayuda a una a encontrar su hogar
+                        </h3>
+                        <span class="slogan__subtitle">Porque las mascotas son los mejores amigos</span>
+                        <router-link :to="{name : 'ListLost'}">Comienza</router-link>
+                    </div>
+                    <div class="logo">
+                        <img src="../assets/img/img-A.png" alt="logo helpet">
+                    </div>
+                </ul>
+            </full-page>
         </section>
         </router-link>
         <router-view></router-view>
@@ -26,10 +40,22 @@
 </template>
 
 <script>
+    import Vue from 'vue'
+    import 'fullpage.js/vendors/scrolloverflow'
+    import VueFullPage from 'vue-fullpage.js'
 
-export default {
-  name: 'HomePage',
-};
+    Vue.use(VueFullPage);
+    export default {
+        name: 'HomePage',
+        data() {
+            return {
+                options: {
+                    menu: '#menu',
+                    anchors: ['page1', 'page2', 'page3'],
+                },
+            }
+        }
+    };
 </script>
 
 <style scoped>
