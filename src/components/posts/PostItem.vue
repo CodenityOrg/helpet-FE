7abb3658e4b0eb60373bb0d48f257562f6b66d58
Angel <aleangrodriguez123@gmail.com>
2019-08-20 20:08:49 -0500

    change post structure

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,44 +1,38 @@
 <template>
     <div class="tarjeta tarjeta--perrrito-perdido" 
         :id="item._id">
-        <div class="tarjeta__titulo">
-            <div class="image__name">
-                <img :src="item.user.profile" alt="foto de perfil">
-                <span>{{fullName}}</span>
-            </div>
-            <button class="btn--show__info" @click="showUserInfo">Ver info</button>
-        </div>
-        <div style="width: 400px; min-height: 50px; max-height: auto; float: left; " class="tarjeta__imagen">
-            <carousel v-if="item.photos && item.photos.length" :perPageCustom="[[1024, 1]]" >
-                <slide
-                    :key="photo._id"
-                    v-for="photo in item.photos" >
-
-                    <div
-                        :style="styles(photo.thumbnailPath)"
-                    >
+        <b-row>
+            <b-col md="4">
+                <div class="tarjeta__imagen">
+                    <img
+                        src="https://saveapetil.org/wp-content/themes/saveapet/images/dog-placeholder.jpg"
+                        alt="perrito encontrado">
+                </div>
+            </b-col>
+            <b-col md="8">
+                <div class="tarjeta__titulo">
+                    <div class="image__name">
+                        <img :src="item.user.profile" alt="foto de perfil">
+                        <span>{{fullName}}</span>
                     </div>
-                </slide>
-            </carousel>
-            <img
-                v-else
-                src="https://saveapetil.org/wp-content/themes/saveapet/images/dog-placeholder.jpg"
-                alt="perrito encontrado">
-        </div>
-        <div class="tarjeta__descripcion">
-            <p class="descripcion"> {{item.description}} </p>
-            <p class="direccion"> {{item.address}} </p>
-            <div class="caracteristicas">
-                <p><b> Características </b> </p>
-                <span
-                    :key="tag._id"
-                    v-for="tag in item.tags"
-                    class="caracteristica">
-                    {{tag.value}}
-                </span>
-                <br/>
-            </div>
-        </div>
+                    <button class="btn--show__info" @click="showUserInfo">Ver info</button>
+                </div>
+                <div class="tarjeta__descripcion">
+                    <p class="descripcion"> {{item.description}} </p>
+                    <p class="direccion"> {{item.address}} </p>
+                    <div class="caracteristicas">
+                        <p><b> Características </b> </p>
+                        <span
+                            :key="tag._id"
+                            v-for="tag in item.tags"
+                            class="caracteristica">
+                            {{tag.value}}
+                        </span>
+                        <br/>
+                    </div>
+                </div>
+            </b-col>
+        </b-row>
     </div>
 </template>
 <script>
