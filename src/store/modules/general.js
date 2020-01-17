6d17f14c9543b4a75595d70850ced29bb2851f6f
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-17 11:16:18 -0500

    call submitContactForm function

--- src/store/modules/general.js
+++ src/store/modules/general.js
@@ -10,7 +10,7 @@ const mutations = {
 
 const actions = {
     async submitContactForm({commit}, form) {
-        const {sent} = await general.submitContactForm(form);
+        const {sent} = await general.default.submitContactForm(form);
         return sent;
     }
 }
