0abfc4eddae01c6f4b7ebdf01b3d9c76097eb9e9
Angel <aleangrodriguez123@gmail.com>
2020-04-04 11:27:16 -0500

    add words for home and publications view

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,20 +1,58 @@
 export default {
   es: {
-    message: {
-      hello: 'Hola Mundo!'
+    home: {
+      introMessage: 'Encuentra a tu mascota perdida o ayuda a una a encontra su hogar',
+      sloganMessage: 'Porque las mascotas son los mejores amigos',
+      startButton: 'Comienza',
+      learnButton: 'Aprende como',
     },
-    custom: {
-      tags: {
-        required: '* Ingresa al menos una característica'
+    navigation: {
+      button1: 'Iniciar sesion',
+      button2: 'Registrate',
+      button3: 'Publicaciones',
+      button4: 'Contacto'
+    },
+    publications: {
+      filterButton: 'Filtrar por...',
+      orderButton: 'Ordenar por...',
+      filters: {
+        losts: 'Perdidos',
+        recent: 'Mas reciente'
       },
-      fotos: {
-        required: '* Elije entre 1 y 3 fotos'
-      }
+      post: {
+        descriptionLabel: 'Descripcion',
+        addressLabel: 'Ultimo lugar visto',
+        characteristic: 'Caracteristicas'
+      },
+      contactButton: 'Contactar'
     }
   },
   en: {
-    message: {
-      hello: 'Hello World!'
+    home: {
+      introMessage: 'Find your lost pet or help one to find its home',
+      sloganMessage: 'Because our pets are the best friends',
+      startButton: 'Start',
+      learnButton: 'Learn how'
+    },
+    navigation: {
+      button1: 'Sign in',
+      button2: 'Register',
+      button3: 'Publications',
+      button4: 'Contact'
+    },
+    publications: {
+      filterButton: 'Filter by...',
+      orderButton: 'Order by...',
+      filters: {
+        losts: 'Losts',
+        recent: 'Recents'
+      }, 
+      post: {
+        descriptionLabel: 'Description',
+        addressLabel: 'Last place viewed',
+        characteristic: 'Characteristics'
+      },
+      contactButton: 'Contact'
     }
   }
 }
